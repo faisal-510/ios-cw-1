4b2e0ff279a085340cefb37ebf7d033f8e035056
faisal-510 <66562768+faisal-510@users.noreply.github.com>
2020-06-13 18:55:05 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let myName = "Faisal"
+var Age = 16
+var height = 170
+var hobby1 = "Photography"
+var hobby2 = "Running"
+
+print("Hello My Name is",myName)
+print("I am",Age,"years old")
+print("I am",height,"cm tall")
+print("My hobbies are",hobby1,"and",hobby2)
+
+
+
