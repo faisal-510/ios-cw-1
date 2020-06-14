55b6fb1c5b0e8acb5d61767479ce8b06b1d0167a
faisal-510 <66562768+faisal-510@users.noreply.github.com>
2020-06-14 12:46:22 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 var str = "Hello, playground"
-
+// part 1
 let myName = "Faisal"
 var Age = 16
 var height = 170
@@ -13,6 +13,7 @@ print("I am",Age,"years old")
 print("I am",height,"cm tall")
 print("My hobbies are",hobby1,"and",hobby2)
 
+// part 2
 let friendName = "Ahmad"
 var friendHeight = 168
 var friendAge = 16
@@ -25,11 +26,17 @@ myName != friendName
 Age == friendAge
 Age >= friendAge
 Age <= friendAge
+Age > friendAge
+Age < friendAge
+Age != friendAge
 
+height < friendHeight
 height > friendHeight
 height >= friendHeight
 height <= friendHeight
 height != friendHeight
+height == friendHeight
+
 
 hobby1 != friendhobby1
 hobby2 != friendhobby2
